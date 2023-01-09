345780b5125fdd34039b60101e5f19d2c94b16b1
Adrikikicp <adrikikicp@gmail.com>
2023-01-08 21:58:17 -0500

    Fix build error

--- src/test/java/techne/Techne.java
+++ src/test/java/techne/Techne.java
@@ -4,7 +4,8 @@ import com.yamishdoy.data.YamishDataFile;
 import net.enriquitomc.Technetice;
 import net.enriquitomc.data.DataPack;
 import org.apache.log4j.LogManager;
-
+import java.io.*;
+import java.net.*;
 public class Techne {
     public void load(){
         LogManager.getLogger("ModelLoader").info("Loaded Technetice from YamishDoy");
